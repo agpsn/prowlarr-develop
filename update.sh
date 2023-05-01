5808f4489970fb6225288fc89f8f87a68e90a1e9
AGPSN <github@agpsn.com>
2023-04-30 20:10:11 -0500

    Updated

--- update.sh
+++ update.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
-set -eu
+clear; set -eu
+
+[ ! -d "/mnt/user/system/agpsn-github/prowlarr-develop" ] && echo "No repo!" && exit 1
+cd "/mnt/user/system/agpsn-github/prowlarr-develop"
 
 echo $(cat ~/.ghcr-token) | docker login ghcr.io -u $(cat ~/.ghcr-user) --password-stdin &>/dev/null
-GBRANCH=$(git branch | grep "*" | rev | cut -f1 -d" " | rev)
+
 PVERSION=$(curl -sL "https://prowlarr.servarr.com/v1/update/develop/changes?runtime=netcore&os=linuxmusl" | jq -r '.[0].version')
 
-if [ $GBRANCH != "develop" ]; then git checkout develop; fi
-	echo "Building and Pushing 'ghcr.io/agpsn/docker-prowlarr:$PVERSION'"
-	docker build --quiet  --force-rm --rm --tag ghcr.io/agpsn/docker-prowlarr:develop --tag ghcr.io/agpsn/docker-prowlarr:$PVERSION -f ./Dockerfile.develop .
-	docker push --quiet ghcr.io/agpsn/docker-prowlarr:develop; docker push --quiet ghcr.io/agpsn/docker-prowlarr:$PVERSION && docker image rm -f ghcr.io/agpsn/docker-prowlarr:$PVERSION
-	git tag -f $PVERSION && git push origin $PVERSION -f --tags
-	echo ""
-	git add . && git commit -m "Updated" && git push --quiet
+echo "Building and Pushing 'ghcr.io/agpsn/docker-prowlarr:$PVERSION'"
+docker build --quiet  --force-rm --rm --tag ghcr.io/agpsn/docker-prowlarr:develop --tag ghcr.io/agpsn/docker-prowlarr:$PVERSION -f ./Dockerfile.develop .
+docker push --quiet ghcr.io/agpsn/docker-prowlarr:develop; docker push --quiet ghcr.io/agpsn/docker-prowlarr:$PVERSION && docker image rm -f ghcr.io/agpsn/docker-prowlarr:$PVERSION
+git tag -f $PVERSION && git push --quiet origin $PVERSION -f --tags
+git add . && git commit -m "Updated" && git push --quiet
